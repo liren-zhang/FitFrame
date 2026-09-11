afb29d97dac5335d22f1d9bae7ce1c3781774f6f
LirenZhang <liren.zhang@student.uts.edu.au>
2026-09-11 10:27:28 +1000

    Initial Commit

---
+++ FitFrame/FitFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFrameApp.swift
+//  FitFrame
+//
+//  Created by Liren Zhang on 11/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct FitFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitFrameTests/FitFrameTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitFrameTests.swift
+//  FitFrameTests
+//
+//  Created by Liren Zhang on 11/9/2026.
+//
+
+import Testing
+@testable import FitFrame
+
+struct FitFrameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
